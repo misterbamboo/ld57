c73e33fa310986200ea209262a86d85d08ad0ee4
Nicolas Charette-Naud <nicolas@bamboosoft.ca>
2025-04-05 16:52:04 -0400

    feat: Shop position to follow waves

--- src/godotproject/Water/water.gd
+++ src/godotproject/Water/water.gd
@@ -61,10 +61,10 @@ func change_depth_color():
 	color = lerp(water_surface_color, water_depth_color, depthRatio)
 	
 func surface_variation(x: float) -> float:
-	var t1 = time * 10 + x
-	var wave1 = sin(t1 * 0.1) * 3
+	var t1 = time * 20 + x
+	var wave1 = sin(t1 * .05) * 2
 	var t2 = time * 2 - x
-	var wave2 = cos(t2 * 0.5) * .4
-	var t3 = time * 0.5 + x
-	var wave3 = sin(t1 * 0.01) * 15
+	var wave2 = cos(t2 * 0.8) * .45
+	var t3 = time * 2 + x
+	var wave3 = sin(t1 * 0.01) * 20
 	return wave1 + wave2 + wave3
