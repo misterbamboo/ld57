08a8b7ced1ac3660034aca47656a8810c4cf256d
Nicolas Charette-Naud <nicolas@bamboosoft.ca>
2025-04-07 14:44:40 -0400

    refactor: Extract NoiseGen

---
+++ src/godotproject/Map/NoiseGen/NoiseGen.gd
@@ -0,0 +1,126 @@
+extends Node
+class_name NoiseGen
+# SEE: Global NoiseGenService
+
+#########
+# Layers
+#########
+var _noiseGenLayers: Array[NoiseGenLayer] = []
+var _layerNames := {}
+func get_layer_count() -> int:
+	return _noiseGenLayers.size()
+func get_layer_name(index: int) -> String:
+	return _layerNames[index]
+func get_layer_with_name(name: String) -> NoiseGenLayer:
+	for layer in _noiseGenLayers:
+		if layer.Name == name:
+			return layer
+	return null
+	
+#########
+# Curves
+#########
+var _curves: Array[Curve] = []
+var _curvesNames := {}
+var _layerIndex_to_curve_index := {}
+func get_curve_count() -> int:
+	return _curves.size()
+func get_curve_name(index: int) -> String:
+	return _curvesNames[index]
+func get_curve_index_from_layer(layer: NoiseGenLayer) -> int:
+	for curve in _curves:
+		if curve.resource_name == layer.CurveName:
+			for curveId in _curvesNames.keys():
+				var curveName = _curvesNames[curveId]
+				if curveName == layer.CurveName:
+					return curveId
+	return 0
+
+#########
+# Noises
+#########
+var _noises: Array[FastNoiseLite] = []
+
+func loadAll():
+	loadLayers()
+	loadCurves()
+	refreshAllNoises()
+
+func loadLayers():
+	var path := "res://Map/NoiseGen/Layers/"
+	var dir := DirAccess.open(path)
+	if dir:
+		dir.list_dir_begin()
+		var file_name = dir.get_next()
+		_noiseGenLayers = []
+		var layerI := 0
+		while file_name != "":
+			if not dir.current_is_dir() and file_name.ends_with(".tres"):
+				var full_path = path + file_name
+				var layer = load(full_path)
+				if layer is NoiseGenLayer:
+					_layerNames[layerI] = layer.Name
+					_noiseGenLayers.append(layer)
+					layerI += 1
+			file_name = dir.get_next()
+			
+func loadCurves():
+	var path := "res://Map/NoiseGen/Curves/"
+	var dir := DirAccess.open(path)
+	if dir:
+		dir.list_dir_begin()
+		var file_name = dir.get_next()
+		_curves = []
+		var curveI := 0
+		while file_name != "":
+			if not dir.current_is_dir() and file_name.ends_with(".tres"):
+				var full_path = path + file_name
+				var curve = load(full_path)
+				if curve is Curve:
+					_curves.append(curve)
+					_curvesNames[curveI] = curve.resource_name
+					curveI += 1
+			file_name = dir.get_next()
+			
+func refreshAllNoises():
+	_noises.clear()
+	for noiseGenLayer in _noiseGenLayers:
+		_noises.append(noiseGenLayer.toNoise())
+
+func get_noise_color(x: float, y: float) -> Color:
+	var best_color: Color = Color(0, 0, 0)
+	var color_in_first_layer: bool = false
+	for i in range(_noises.size()):
+		var value = _noises[i].get_noise_2d(x, y)
+		value = (value + 1.0) / 2.0
+		if value <= 0:
+			continue
+		var layer := _noiseGenLayers[i]
+		var curve := _get_curve_from_layer_index(i)
+		var curveVal = curve.sample(y)
+		value = curveVal * value
+		value = clamp(value, 0.0, 1.0)
+		if layer.BreakPointActive:
+			value = 0 if value < layer.BreakPoint else 1
+		if value > 0:
+			if i == 0:
+				color_in_first_layer = true
+			if color_in_first_layer:
+				best_color = Color(0, 0, 0).lerp(layer.LayerColor, value)
+	return best_color
+
+func _get_curve_from_layer_index(layerIndex: int) -> Curve:
+	if _layerIndex_to_curve_index.keys().size() <= 0:
+		_rebuild_layer_to_curve_index()
+	var curveIndex = _layerIndex_to_curve_index[layerIndex]
+	return _curves[curveIndex]
+
+func _rebuild_layer_to_curve_index():
+	_layerIndex_to_curve_index = {}
+	for l in range(get_layer_count()):
+		var layer := _noiseGenLayers[l]
+		for c in range(_curves.size()):
+			var curve := _curves[c]
+			if curve.resource_name == layer.CurveName:
+				_layerIndex_to_curve_index[l] = c
+				break
--- src/godotproject/Scenes/MapGenTestsScene/sliders.gd
+++ src/godotproject/Scenes/MapGenTestsScene/sliders.gd
@@ -7,23 +7,12 @@ class_name NoisePreviewContainer
 var scanLineY: int = 0
 
 var previewSum: int
-var noiseGenLayers: Array[NoiseGenLayer] = []
-
-var noises: Array[FastNoiseLite] = []
-var curves: Array[Curve] = []
 
 var size = 275
 var img := Image.create(size, size, false, Image.FORMAT_RGBA8) # .FORMAT_L8 grayscale
 
-func _on_noise_gen_configs_noise_gen_layers_changed(newLayers: Array[NoiseGenLayer], curvesDef: Array[Curve]) -> void:
-	noiseGenLayers = newLayers
-	curves = curvesDef
-	refreshAllNoises()
-	
-func refreshAllNoises():
-	noises.clear()
-	for noiseGenLayer in noiseGenLayers:
-		noises.append(noiseGenLayer.toNoise())
+func _on_noise_gen_configs_noise_gen_layers_changed() -> void:
+	NoiseGenService.refreshAllNoises()
 
 func _process(time: float):
 	updatePreview()
@@ -32,38 +21,10 @@ func updatePreview():
 	for x in size:
 		for y in size:
 			if y >= scanLineY and y < scanLineY + 10 :
-				var color = _get_noise_color(x, y + yOffset.value)
+				var color = NoiseGenService.get_noise_color(x, y + yOffset.value)
 				img.set_pixel(x, y, color)
 	var tex := ImageTexture.create_from_image(img)
 	noisePreview.texture = tex
 	scanLineY += 10
 	if scanLineY >= size:
 		scanLineY = 0
-
-func _get_noise_color(x: float, y: float) -> Color:
-	var best_color: Color = Color(0, 0, 0)
-	var color_in_first_layer: bool = false
-	for i in range(noises.size()):
-		var value = noises[i].get_noise_2d(x, y)
-		value = (value + 1.0) / 2.0
-		if value <= 0:
-			continue
-		var layer := noiseGenLayers[i]
-		var curve = _find_curve(layer.CurveName)
-		var curveVal = curve.sample(y)
-		value = curveVal * value
-		value = clamp(value, 0.0, 1.0)
-		if layer.BreakPointActive:
-			value = 0 if value < layer.BreakPoint else 1
-		if value > 0:
-			if i == 0:
-				color_in_first_layer = true
-			if color_in_first_layer:
-				best_color = Color(0, 0, 0).lerp(layer.LayerColor, value)
-	return best_color
-
-func _find_curve(curveName: String) -> Curve:
-	for curve in curves:
-		if curve.resource_name == curveName:
-			return curve
-	return null
